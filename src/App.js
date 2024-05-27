6a134a13936077a1d06b45da1a291e7f362afef1
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-05-27 12:01:22 +0530

    update menu

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import CircleWatch from './component/circle';
 import Navbar from './component/nav/Navbar';
 import Watch from './component/watch';
 import { useState } from 'react';
+import HelpIcon from '@mui/icons-material/Help';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 function App() {
   const [True,setTrue]=useState(true)
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
 const handleClick = () => {
   setTrue(!True);
 };
   return (
     <div className="App">
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isOpen ? <CloseIcon /> : <MenuIcon />}
+      </div>
+      <div className={isOpen ? 'nav-menu open' : 'nav-menu'}>
+
       <Navbar/>
+      </div>
       {True ? <CircleWatch/> : <Watch/>}
       <div className="chenge">
       <button class='btn btn-custom mt-3' onClick={handleClick}>chenge clock Design</button>
--- src/component/nav/Navbar.jsx
+++ src/component/nav/Navbar.jsx
@@ -1,83 +1,77 @@
 // Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-
 import HomeIcon from '@mui/icons-material/Home';
-import EmailIcon from '@mui/icons-material/Email';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import ImageIcon from '@mui/icons-material/Image';
+import FileIcon from '@mui/icons-material/Description';
+import GameIcon from '@mui/icons-material/SportsEsports';
+import BookIcon from '@mui/icons-material/MenuBook';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import SettingsIcon from '@mui/icons-material/Settings';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import PersonIcon from '@mui/icons-material/Person';
 
-function Navbar() {
+
+const Navbar = () => {
+ 
+
+  
+
   return (
-    <div className='nav' >
+    <div className="nav">
+      
       <ul>
         <li>
           <a href="#">
-          <HomeIcon />
+            <HomeIcon />
             <span>Dashboard</span>
           </a>
         </li>
         <li>
           <a href="#">
-          <InstagramIcon /> 
-            <span>Instagram</span>
+            <ImageIcon />
+            <span>Images</span>
           </a>
         </li>
         <li>
           <a href="#">
-          <EmailIcon />
-            <span>Email</span>
+            <FileIcon />
+            <span>Files</span>
           </a>
         </li>
         <li>
           <a href="#">
-          <FacebookIcon /> 
-            <span>Facebook</span>
+            <GameIcon />
+            <span>Games</span>
           </a>
         </li>
         <li>
           <a href="#">
-          <NotificationsIcon /> 
+            <BookIcon />
+            <span>Books</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <NotificationsIcon />
             <span>Notifications</span>
           </a>
         </li>
-        
         <li>
           <a href="#">
-          <AccountCircleIcon /> 
-            <span>Profile</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-          <GitHubIcon /> 
-            <span>GitHub</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-    
-    <WhatsAppIcon /> 
-            <span>WhatsApp</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-          <SettingsIcon /> 
+            <SettingsIcon />
             <span>Settings</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <PersonIcon />
+            <span>Profile</span>
           </a>
         </li>
       </ul>
 
-
-      
     </div>
   );
-}
+};
 
 export default Navbar;
